ed4bd8b862b0309340b7944d80a19f59345c1d84
Krystal Pérez <kperez2760@arecibointer.edu>
2024-03-02 20:10:38 -0400

    New File

---
+++ Laborarios_C++/.vscode/holaMundo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main() 
+{
+
+string nombre;
+
+cout << "Ingrese su nombre: ";
+cin >> nombre;
+
+for (int i = 0; i < 5; i++ ) 
+{
+    cout << nombre << endl;
+}
+}
---
+++ Laborarios_C++/.vscode/nada.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main() 
+{
+
+cout << "Krystal";
+return 0;
+}
