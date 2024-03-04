1216670fcc63d721968bc2780ca46fab4205e5e1
Krystal Pérez <kperez2760@arecibointer.edu>
2024-03-04 18:57:11 -0400

    New File

---
+++ LabsC#/EjemplosVariables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EjemplosVariables
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int myVariable = 10;
+            const int myConstant = 20;
+
+            Console.WriteLine("Valor de mi variable: "
+            + myVariable);
+            Console.WriteLine("Valor de mi constante: "
+            + myConstant);
+        }
+    }
+}
