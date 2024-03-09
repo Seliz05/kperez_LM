735a107588abcbe6d3c3c9129b711d03f1708416
Krystal Pérez <kperez2760@arecibointer.edu>
2024-03-09 14:51:48 -0400

    New File

--- LabsC#/EjemploCaptura/obj/Debug/net8.0/EjemploCaptura.AssemblyInfo.cs
+++ LabsC#/EjemploCaptura/obj/Debug/net8.0/EjemploCaptura.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjemploCaptura")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1216670fcc63d721968bc2780ca46fab4205e5e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a9b11c2b163ab78f5065e525482afadec28f5fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjemploCaptura")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjemploCaptura")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
