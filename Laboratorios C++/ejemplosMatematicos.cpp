ab445f9e720add5a6e988d345b475ab07ec432fa
Krystal Pérez <kperez2760@arecibointer.edu>
2024-03-11 09:46:10 -0400

    New File

---
+++ Laboratorios C++/ejemplosMatematicos.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double num1, num2, media;
+
+    cout << "Introduzca el primer número: ";
+    cin >> num1;
+
+    cout << "Introduzca el segundo número: ";
+    cin >> num2;
+
+    media = (num1 + num2)/2;
+
+    cout << "La madia es: " << media << endl;
+
+    return 0;
+}
---
+++ LabsC#/EjemplosMatematicos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EjemplosMatematicos
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double num1, num2, media;
+            
+            Console.Write("Introduzca el primer número: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("INtroduzca el segundo número: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            media = (num1 + num2)/2;
+
+            Console.WriteLine("La media es: " + media);
+        }
+    }
+}
