1c376435409fe4a4a575808d7fdd2648fe3fbc3e
Krystal Pérez <kperez2760@arecibointer.edu>
2024-04-22 07:59:04 -0400

    Update File

--- LabsC#/Mod05Act04While/Program.cs
+++ LabsC#/Mod05Act04While/Program.cs
@@ -22,15 +22,15 @@ namespace Mod05Act04While
             while(i <= numeroEmpleados)//ciclo que permite que se calcule el sueldo bruto y neto de 3 empleados con un descuento de %7
             {
                 Console.Write("Ingrese el pago por hora del empleado #" +i+ ": ");//entrada de valores
-                pagoHora[i - 1] = float.Parse(Console.ReadLine());//captura de valores para cada empleado utilizando [i]
+                pagoHora[i - 1] = float.Parse(Console.ReadLine());//captura de valores para cada empleado utilizando [i - 1]
 
                 Console.Write("Ingrese las horas trabajadas del empleado #" +i+ ": ");//entrada de valores
-                horasTrabajadas[i - 1] = float.Parse(Console.ReadLine());//captura de valores para cada empleado utilizando [i]
+                horasTrabajadas[i - 1] = float.Parse(Console.ReadLine());//captura de valores para cada empleado utilizando [i - 1]
 
                 if (horasTrabajadas[i - 1] > 40)//si el empleado trabaja tiempo extra se le calcula el pago extra por esas horas que trabajo de más
                 {
                     overTimeTrabajado[i - 1] = horasTrabajadas[i - 1] - 40;//saber cuantas horas trabajo extra
-                    overTimePagoHora[i - 1] = pagoHora[i] * 2.0f;//dublicar el pago dependiendo de cuantas horas halla trabajado de más
+                    overTimePagoHora[i - 1] = pagoHora[i - 1] * 2.0f;//dublicar el pago dependiendo de cuantas horas halla trabajado de más
                     sueldoBruto[i - 1] = (40 * pagoHora[i - 1]) + (overTimeTrabajado[i - 1] * overTimePagoHora[i - 1]);//sumar el pago de las horas extras a su sueldo
                 }
                 else//si no trabaja horas extras
@@ -40,26 +40,23 @@ namespace Mod05Act04While
                     overTimePagoHora[i - 1] = 0.0f;
                 }
                 descuento[i - 1] = sueldoBruto[i - 1] * 0.07f;//se aplica el descuento del 7% multiplicandolo al suldo bruto de cada empleado  
-                sueldoNeto[i - 1] = sueldoBruto[i - 1] - descuento[i];//se calcula el sueldo neto restando el suldo bruto y el descuento calculado anteriormente
+                sueldoNeto[i - 1] = sueldoBruto[i - 1] - descuento[i - 1];//se calcula el sueldo neto restando el suldo bruto y el descuento calculado anteriormente
 
-                totalSueldoBruto += sueldoBruto[i];//sumando el sueldo bruto de cada empleado para obtener el total
-                totalOverTimePago += overTimeTrabajado[i] * overTimePagoHora[i];//multiplicar el tiempo y pago extra de los empleados para obtener el total
-                totalOverTimeHoras += overTimeTrabajado[i];//obtener el total de las horas extras trabajadas
+                totalSueldoBruto += sueldoBruto[i -1];//sumando el sueldo bruto de cada empleado para obtener el total
+                totalOverTimePago += overTimeTrabajado[i - 1] * overTimePagoHora[i - 1];//multiplicar el tiempo y pago extra de los empleados para obtener el total
+                totalOverTimeHoras += overTimeTrabajado[i - 1];//obtener el total de las horas extras trabajadas
 
                 Console.WriteLine("Nómina del empleado #" +i+ ": ");//mensaje a implrimir
                 //fixed es para que los valores como sueldoBruto y sueldoNeto se impliman con sus respectivos valores después del punto decimal
                 Console.WriteLine("Sueldo bruto del empleado #" +i+ " es de $" +sueldoBruto[i - 1].ToString("F2")+ " y el sueldo neto es de $" +sueldoNeto[i - 1].ToString("F2"));
-            
-
-                Console.WriteLine("Total pagado en nómina: $" +totalSueldoBruto.ToString("F2"));//mensajes a imprimir
-
-                Console.WriteLine("Total pagado en horas extra: $" +totalOverTimePago.ToString("F2"));
-            
-                Console.WriteLine("Total de horas extra trabajadas: " +totalOverTimeHoras.ToString("F2")+ "horas");
                 
                 i++;//suma de la variable i
             }  
+            Console.WriteLine("Total pagado en nómina: $" +totalSueldoBruto.ToString("F2"));//mensajes a imprimir
+
+            Console.WriteLine("Total pagado en horas extra: $" +totalOverTimePago.ToString("F2"));
             
+            Console.WriteLine("Total de horas extra trabajadas: " +totalOverTimeHoras.ToString("F2")+ "horas");
         }
     }
 }
--- LabsC#/Mod05Act04While/obj/Debug/net8.0/Mod05Act04While.AssemblyInfo.cs
+++ LabsC#/Mod05Act04While/obj/Debug/net8.0/Mod05Act04While.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mod05Act04While")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a95230e62eded6740a5294b8946e00f61a430356")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2570216470b506b8078ad01381fae090937e544a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mod05Act04While")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mod05Act04While")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
